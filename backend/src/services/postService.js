26c28e2133ca16f55f236246131bb9d29dc08f6e
Alif <sagor01md@gmail.com>
2024-05-18 21:40:51 +0600

    [ADD] SERVICES: post function skeletons

---
+++ backend/src/services/postService.js
@@ -0,0 +1,31 @@
+const postModel = require('../models/postModel')
+
+// create post
+const createPost = async (req) => {
+
+}
+
+// update post
+const updatePost = async (req) => {
+
+}
+
+// delete post
+const deletePost = async (req) => {
+
+}
+
+// get a single post
+const getSinglePost = async (req) => {
+
+}
+
+// get a lot of posts
+const getLotOfPosts = async (req) => {
+
+}
+
+// report a post
+const reportPost = async (req) => {
+
+}
