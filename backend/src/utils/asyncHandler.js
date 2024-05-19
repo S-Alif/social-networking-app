a4e198a21c29473823d3446ab582427f5ff39407
Alif <sagor01md@gmail.com>
2024-05-19 22:38:46 +0600

    [FIX] asyncHandler not returning error message

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,12 +1,11 @@
 // async handler function to reduce writing try catch blocks every time
 
-module.exports = (fn) => {
-  return async (...args) => {
-    try {
-      return await fn(...args)
-    } catch (error) {
-      console.error(error); 
-      return {status: 0, code: 200, data: "something went wrong"}
-    }
+module.exports = async (fn) => {
+  try {
+    let result = await fn()
+    return result
+  } catch (error) {
+    console.error(error);
+    return { status: 0, code: 200, data: "something went wrong" }
   }
 } 
