5c9e127acc149af7d42a67f8cab0c86dce39cb6f
Alif <sagor01md@gmail.com>
2024-05-22 00:14:19 +0600

    [UPDATE] MODELS: otp model - otp expires after a duration of time

--- backend/src/models/otpModel.js
+++ backend/src/models/otpModel.js
@@ -4,7 +4,7 @@ const otpSchema = mongoose.Schema({
   email: { type: String, required: true },
   otp: { type: String, required: true },
   verified: { type: Boolean, default: 0 },
-  expired: { type: Boolean, default: 0 }
+  createdAt: { type: Date, default: Date.now, index: { expires: '2m' } },
 }, { timestamps: true, versionKey: false });
 
 module.exports = mongoose.model('otps', otpSchema)
