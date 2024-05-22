b8a995af5abd77fe1e2cfed133aa2bf3029f3183
Alif <sagor01md@gmail.com>
2024-05-22 20:16:31 +0600

    [UPDATE] CONTROLLERS: post controller update in res to asyncHandler

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -1,38 +1,38 @@
-const posetService = require('../services/postService')
+const postService = require('../services/postService')
 const asyncHandler = require('../utils/asyncHandler')
 
 // create post
-exports.postCreate = asyncHandler(async (req, res) => {
-  let result = await posetService.createPost(req)
+exports.postCreate = async (req, res) => {
+  let result = await asyncHandler(() => postService.createPost(req))
   res.status(200).json(result)
-})
+}
 
 // update post
-exports.postUpdate = asyncHandler(async (req, res) => {
-  let result = await posetService.updatePost(req)
+exports.postUpdate = async (req, res) => {
+  let result = await asyncHandler(() => postService.updatePost(req))
   res.status(200).json(result)
-})
+}
 
 // delete post
-exports.postDelete = asyncHandler(async (req, res) => {
-  let result = await posetService.deletePost(req)
+exports.postDelete = async (req, res) => {
+  let result = await asyncHandler(() => postService.deletePost(req))
   res.status(200).json(result)
-})
+}
 
 // get a single post
-exports.singlePost = asyncHandler(async (req, res) => {
-  let result = await posetService.getSinglePost(req)
+exports.singlePost = async (req, res) => {
+  let result = await asyncHandler(() => postService.getSinglePost(req))
   res.status(200).json(result)
-})
+}
 
 // get a lot of posts
-exports.lotOfPosts = asyncHandler(async (req, res) => {
-  let result = await posetService.getLotOfPosts(req)
+exports.lotOfPosts = async (req, res) => {
+  let result = await asyncHandler(() => postService.getLotOfPosts(req))
   res.status(200).json(result)
-})
+}
 
 // report a post
-exports.postReport = asyncHandler(async (req, res) => {
-  let result = await posetService.reportPost(req)
+exports.postReport = async (req, res) => {
+  let result = await asyncHandler(() => postService.reportPost(req))
   res.status(200).json(result)
-})
+}
