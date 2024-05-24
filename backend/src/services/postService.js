f1d4eb8eb821366e81e7dbb6161c779dbdf6f059
Alif <sagor01md@gmail.com>
2024-05-24 11:59:34 +0600

    [DONE] SERVICES: post creation

--- backend/src/services/postService.js
+++ backend/src/services/postService.js
@@ -1,21 +1,68 @@
 const postModel = require('../models/postModel');
+const attachmentModel = require('../models/postAttachmentModel');
 const { responseMsg } = require('../utils/helpers');
-const { imageUploader } = require('../utils/postAttachmentUploader');
+const { imageUploader, deleteFiles } = require('../utils/postAttachmentUploader');
 
 // create post
 exports.createPost = async (req) => {
 
+  if (!req?.files && !req.body?.caption) return responseMsg(0, 200, "No data to post");
+
   let urlArray = []
+  let fileTypeArray = []
 
-  const promises = Object.keys(req.files).flatMap(key => {
-    const fileArray = Array.isArray(req.files[key]) ? req.files[key] : [req.files[key]];
+  // Upload files to Cloudinary if they exist
+  if (req.files) {
+    try {
+      const promises = Object.keys(req.files).flatMap(key => {
+        const fileArray = Array.isArray(req.files[key]) ? req.files[key] : [req.files[key]]
 
-    return fileArray.map(file => imageUploader(file));
-  });
+        fileArray.forEach(file => fileTypeArray.push(file.mimetype))
+        return fileArray.map(file => imageUploader(file))
+      })
 
-  urlArray = await Promise.all(promises)
+      urlArray = await Promise.all(promises);
+      if (urlArray.includes(null)) {
+        await deleteFiles(urlArray.filter(url => url !== null))
+        return responseMsg(0, 200, "Could not upload files")
+      }
+    } catch (error) {
+      return responseMsg(0, 200, "An error occurred while uploading attachments")
+    }
+  }
 
-  return responseMsg(1, 200, urlArray)
+
+  // Create a post
+  var createPost;
+  try {
+    createPost = await postModel.create({ author: req.headers.id, caption: req.body?.caption || "", postType: req.body?.postType })
+    if (!createPost) throw new Error("Post creation failed")
+    
+  } catch (error) {
+    await deleteFiles(urlArray)
+    return responseMsg(0, 200, "Could not post")
+  }
+
+  // Upload the file URLs to the database
+  try {
+    if (urlArray.length > 0) {
+      await Promise.all(
+        urlArray.map(async (e, index) => {
+          await attachmentModel.create({
+            postId: createPost?._id,
+            fileLocation: e,
+            fileType: fileTypeArray[index].split('/')[0] === "image" ? "image" : "video"
+          })
+        })
+      )
+    }
+  } catch (error) {
+    await postModel.deleteOne({ _id: createPost?._id })
+    await deleteFiles(urlArray)
+    return responseMsg(0, 200, "An error occurred while saving attachments")
+  }
+
+  return responseMsg(1, 200, "post uploaded successfully")
 }
 
 // update post
