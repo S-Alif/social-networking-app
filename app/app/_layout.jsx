64831453e2fb1b7d1ca741b9f068775d874a7fc1
Alif <sagor01md@gmail.com>
2024-05-25 00:09:44 +0600

    [DONE] APP: initial setup

---
+++ app/app/_layout.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RootLayout = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>RootLayout</Text>
+    </View>
+  )
+}
+
+export default RootLayout
