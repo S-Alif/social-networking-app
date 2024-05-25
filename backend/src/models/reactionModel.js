d4ae28274fe6483d70bb6d1f845cc697b9a36820
Alif <sagor01md@gmail.com>
2024-05-25 21:53:33 +0600

    [UPDATE] MODELS: reaction models set predefined reactions

--- backend/src/models/reactionModel.js
+++ backend/src/models/reactionModel.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const reactionSchema = mongoose.Schema({
   author: { type: mongoose.Types.ObjectId, required: true, ref: "users" },
   reactedOn: { type: mongoose.Types.ObjectId, required: true, },
-  reaction: { type: Boolean, default: 0 }
+  reaction: {
+    type: String,
+    enum: ['👍', '😂', '❤️', '😲', '😢', '😡'], 
+    required: true
+  }
 }, { timestamps: true, versionKey: false });
 
 module.exports = mongoose.model('reactions', reactionSchema)
