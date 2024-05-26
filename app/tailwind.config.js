24905fa3f1ea14a5d42016cb3ee7da91f3047d3f
Alif <sagor01md@gmail.com>
2024-05-26 21:29:09 +0600

    [DONE] APP: tailwind extended theme config

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#161622",
+        purpleColor: "#6835F0",
+        pinkColor: "#FF0074",
+        browneColor: "#545454",
+        lightGrayColor: "#E7E7E7",
+        redColor: "#FE0D0D",
+        darkGrayColor: "#3D4754",
+        grayColor: "#71777F",
+        greenColor: "#37BD4E",
+        lightGrayColor2: "#F3F6F6",
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
