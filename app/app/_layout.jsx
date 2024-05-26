ca5bac3f65bdce61adbef00374ce28a03340db22
Alif <sagor01md@gmail.com>
2024-05-26 22:09:31 +0600

    [DONE] APP: root layout

--- app/app/_layout.jsx
+++ app/app/_layout.jsx
@@ -1,11 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+
+SplashScreen.preventAutoHideAsync()
 
 const RootLayout = () => {
+
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  // on font loaded or changed
+  useEffect(() => {
+
+    if (error) throw error
+    if (fontsLoaded) SplashScreen.hideAsync()
+
+  }, [fontsLoaded, error])
+
+  if (!fontsLoaded && !error) return null
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>RootLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen name='index' options={{ headerShown: false }} />
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar animated={true} backgroundColor='#F3F6F6' style='dark'  />
+    </>
   )
 }
 
