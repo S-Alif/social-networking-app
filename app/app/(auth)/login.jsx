ae86c81132d2c441bd101f672194a308b0b9e3e4
Alif <sagor01md@gmail.com>
2024-05-28 01:47:02 +0600

    [DONE] APP : Login design 70%

--- app/app/(auth)/login.jsx
+++ app/app/(auth)/login.jsx
@@ -1,11 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
+import AuthTabScreen from './../../components/authTabScreen';
+import FormTextInput from '../../components/textInput';
 
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [pass, setPass] = useState("")
+
+  console.log(email, pass)
+
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <AuthTabScreen>
+
+      <Text className="text-5xl pt-10 font-pbold">Welcome back !!</Text>
+      <Text className="text-2xl text-grayColor">We've missed you</Text>
+
+      {/* form inputs */}
+      <View className="flex-1 mt-10">
+
+        <FormTextInput
+          title={"Email"}
+          placeholder={"Enter your email"}
+          validationMsg={"Enter a valid email"}
+          value={setEmail}
+          regex={/^[a-zA-Z0-9._%+-]+@(gmail\.com|yahoo\.com|hotmail\.com)$/}
+        />
+
+        <FormTextInput
+          title={"Password"}
+          placeholder={"Enter your password"}
+          validationMsg={"password should be of eight characters"}
+          value={setPass}
+          regex={/^.{8,}$/}
+        />
+
+      </View>
+    </AuthTabScreen>
   )
 }
 
