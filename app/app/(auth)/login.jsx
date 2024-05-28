b0331d354fb203dd3e9fe00fcd929bb492843b71
Alif <sagor01md@gmail.com>
2024-05-28 22:21:59 +0600

    [DONE] APP: login screen and install axios

--- app/app/(auth)/login.jsx
+++ app/app/(auth)/login.jsx
@@ -2,13 +2,30 @@ import { View, Text, SafeAreaView } from 'react-native'
 import React, { useState } from 'react'
 import AuthTabScreen from './../../components/authTabScreen';
 import FormTextInput from '../../components/textInput';
+import CustomButton from '../../components/CustomButton';
+import { customAlert } from '../../scripts/alerts';
+import { Link } from 'expo-router';
+import api from '../../scripts/api';
+import { userUrl } from '../../scripts/endpoints';
+import * as SecureStore from 'expo-secure-store'
+import { dataSender } from '../../scripts/apiCaller';
 
 const Login = () => {
 
   const [email, setEmail] = useState("")
   const [pass, setPass] = useState("")
+  const [loading, setLoading] = useState(false)
 
-  console.log(email, pass)
+  const handleLogin = async () => {
+    if (email == "" || pass == "") return customAlert("ERROR !!", "All data is required")
+    setLoading(true)
+    let result = await dataSender(userUrl + "/login", { email, pass })
+    if(result != null) {
+      await SecureStore.setItemAsync('token', result.token)
+    }
+    setLoading(false)
+  }
+
 
   return (
     <AuthTabScreen>
@@ -34,6 +51,27 @@ const Login = () => {
           value={setPass}
           regex={/^.{8,}$/}
         />
+
+        {/* forgot password */}
+        <Text className="mt-10 font-psemibold text-lg text-purpleColor">Forgot Password ?</Text>
+
+        {/* buttons */}
+        <CustomButton
+          title={"Log in"}
+          handlePress={handleLogin}
+          isloading={loading}
+          containerStyles={"bg-purpleColor min-h-[50] my-10"}
+          textStyles={"text-lightGrayColor2 text-xl font-pmedium"}
+        />
+
+        {/* go to regsiter page */}
+        <View className="justify-center pt-5 flex-row gap-2">
+          <Text className="text-lg font-pmedium text-darkGrayColor">
+            Don't have account ?
+          </Text>
+
+          <Link href={"/register"} className='text-lg font-psemibold text-purpleColor'>Sign Up</Link>
+        </View>
 
       </View>
     </AuthTabScreen>
