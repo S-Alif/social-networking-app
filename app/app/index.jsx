259f23e481a7f8076ef96397f5ee87f9eb90fa69
Alif <sagor01md@gmail.com>
2024-05-28 22:21:19 +0600

    [DONE] APP: first screen

--- app/app/index.jsx
+++ app/app/index.jsx
@@ -1,17 +1,33 @@
-import { View, Text, SafeAreaView, ImageBackground, Image } from 'react-native'
 import React, { useEffect } from 'react'
+import { View, Text, SafeAreaView, ImageBackground, Image } from 'react-native'
 import * as SecureStore from 'expo-secure-store'
 import logo3 from '../assets/images/connect_vive_logo_3.png'
 import { router } from 'expo-router'
 import AuthTabScreen from '../components/authTabScreen'
+import { userUrl } from '../scripts/endpoints'
+import authStore from '../constants/authStore'
+import api from '../scripts/api'
+import { dataFetcher } from '../scripts/apiCaller'
 
 const Index = () => {
+
+  const {setProfile} = authStore()
 
   // route based on token
   useEffect(() => {
     setTimeout(async () => {
       let token = await SecureStore.getItemAsync('token') || null
-      if (!token) router.replace('/login')
+      if (!token) {
+        router.replace('/login')
+        return
+      }
+
+      let result = await dataFetcher(userUrl + '/profile')
+
+      if (result != null) {
+        return setProfile(result?.data)
+      }
+      setProfile(null)
     }, 2000)
   }, [])
 
