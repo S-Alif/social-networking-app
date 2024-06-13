675b241252ee783c73e7038aba63553e7abfca68
Alif <sagor01md@gmail.com>
2024-06-13 19:56:20 +0600

    [UPDATE] APP: change in applying header styles

--- app/app/pages/_layout.jsx
+++ app/app/pages/_layout.jsx
@@ -3,38 +3,32 @@ import React from 'react'
 import { Stack } from 'expo-router'
 
 const PageLayout = () => {
+
+  const headerStyles = {
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 25,
+  }
+
   return (
     <Stack>
       <Stack.Screen name='profile' options={{
         headerTitle: "Profile",
-        headerTitleStyle: {
-          fontFamily: "Poppins-SemiBold",
-          fontSize: 25,
-        }
+        headerTitleStyle: headerStyles
       }} />
 
       <Stack.Screen name='showComments' options={{
         headerTitle: "Comments",
-        headerTitleStyle: {
-          fontFamily: "Poppins-SemiBold",
-          fontSize: 25,
-        }
+        headerTitleStyle: headerStyles
       }} />
 
       <Stack.Screen name='updateComment' options={{
         headerTitle: "Update comments",
-        headerTitleStyle: {
-          fontFamily: "Poppins-SemiBold",
-          fontSize: 25,
-        }
+        headerTitleStyle: headerStyles
       }} />
 
       <Stack.Screen name='userProfileById' options={{
         headerTitle: "",
-        headerTitleStyle: {
-          fontFamily: "Poppins-SemiBold",
-          fontSize: 25,
-        }
+        headerTitleStyle: headerStyles
       }} />
 
     </Stack>
