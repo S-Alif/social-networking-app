50ff949c5b65f79944e6e278f30b8d8ed9064bb8
Alif <sagor01md@gmail.com>
2024-06-15 23:26:54 +0600

    [ADD] APP: new date formatters

--- app/scripts/dateFormatter.js
+++ app/scripts/dateFormatter.js
@@ -1,7 +1,17 @@
 import moment from 'moment-timezone';
 
 export const formatDate = (datetime) => {
-  const userTimeZone = moment.tz.guess();
-  const zonedDate = moment.tz(datetime, userTimeZone);
-  return zonedDate.format('DD-MM-YYYY [at] h:mm A');
-};
+  const userTimeZone = moment.tz.guess()
+  const zonedDate = moment.tz(datetime, userTimeZone)
+  return zonedDate.format('DD-MM-YYYY [at] h:mm A')
+}
+
+export const formatOnlyDate = (datetime) => {
+  const userTimeZone = moment.tz.guess()
+  const zonedDate = moment.tz(datetime, userTimeZone)
+  return zonedDate.format('DD-MM-YYYY')
+}
+
+export const formatTimestamp = (date) => {
+  return moment(date).utc().format()
+}
