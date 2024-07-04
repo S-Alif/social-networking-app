f93beace55e054ea55a52e39312ef77a3cb9cb45
Alif <sagor01md@gmail.com>
2024-07-04 12:51:06 +0600

    [DONE] Single post page

--- app/app/pages/SinglePost.jsx
+++ app/app/pages/SinglePost.jsx
@@ -1,12 +1,11 @@
-import { View, Text, TextInput, FlatList } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { Stack, useLocalSearchParams } from 'expo-router'
-import { dataFetcher } from '../../scripts/apiCaller'
+import { View, Text, TextInput, ScrollView, ActivityIndicator } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { Stack, useFocusEffect, useLocalSearchParams } from 'expo-router'
+import { dataFetcher, reactionSender } from '../../scripts/apiCaller'
 import { commentUrl, postUrl } from '../../scripts/endpoints'
 import PostCards from '../../components/postCards'
 import CustomButton from './../../components/CustomButton';
 import CommentCard from '../../components/commentCard'
-import { Entypo } from '@expo/vector-icons';
 
 const SinglePost = () => {
 
@@ -16,13 +15,23 @@ const SinglePost = () => {
   const [data, setData] = useState(null)
   const [comments, setComments] = useState([])
   const [page, setPage] = useState(1)
-  const [newComment, setNewComment] = useState("")
+  const [newComment, setNewComment] = useState({
+    commentOn: params?.postId,
+    comment: ""
+  })
+  const [refresh, setRefresh] = useState(false)
+  const [firstLoad, setFirstLoad] = useState(true)
 
   // fetch comments function
-  const fetchComments = async () => {
-    let result = await dataFetcher(`${commentUrl}/${params?.postId}/${page}/20`)
+  const fetchComments = async (pageNum) => {
+    let result = await dataFetcher(`${commentUrl}/${params?.postId}/${pageNum}/10`)
     if (result == null || result?.status == 0) return
-    setComments(result?.data)
+    if (refresh) {
+      console.log(refresh, "refresh")
+      setRefresh(false)
+      return setComments(result?.data)
+    }
+    setComments(prev => [...prev, ...result?.data])
   }
 
   // fetch the post
@@ -39,57 +48,99 @@ const SinglePost = () => {
   // fetch the comments
   useEffect(() => {
     (async () => {
-      await fetchComments()
+      await fetchComments(page)
+      if (firstLoad) {
+        setFirstLoad(false)
+        console.log("hello")
+      }
     })()
   }, [page])
+
+  useEffect(() => {
+    if (refresh) {
+      (async () => {
+        if (page == 1) return await fetchComments(1)
+        setPage(1)
+      })()
+    }
+  }, [refresh])
+
+  useFocusEffect(useCallback(() => {
+    setRefresh(true)
+  }, []))
+
+
+  // post a comment
+  const handlePostComment = async () => {
+
+    if (newComment.comment.trim() == '') return
+    setLoading(true)
+    let comment = await reactionSender(commentUrl, newComment)
+    setLoading(false)
+    if (comment?.status == 0 || comment == null) return customAlert('ERROR !!', "Could not post your comment")
+    setNewComment({
+      commentOn: params?.postId,
+      comment: ""
+    })
+    setRefresh(true)
+  }
+
+  // handle deleted comment
+  const handleDeletedComment = (e) => {
+    setComments(prev => prev.filter(comment => comment._id !== e))
+  }
+
 
   return (
     <View className="flex-1 bg-lightGrayColor">
 
       {data != null && <Stack.Screen options={{ headerTitle: data?.authorDetails?.firstName + data?.authorDetails?.lastName, headerTitleStyle: { fontSize: 20, fontFamily: "Poppins-Medium" } }} />}
 
-      <FlatList
-        data={comments}
-        keyExtractor={(item) => item?._id}
-        extraData={comments}
-        renderItem={({ item }) => (
-          <CommentCard postId={params?.postId} postAuthor={data?.author} comment={item} />
-        )}
-        ListHeaderComponent={() => (
-          <View className="px-2">
-            {
-              data != null &&
-              <>
-                <PostCards post={data} />
+      <ScrollView
+        contentContainerStyle={{ paddingBottom: 20 }}
+      >
+        {/* the post */}
+        <View className="flex-1 px-2">
+          {
+            data != null &&
+            <>
+              <PostCards post={data} />
 
-                {/* comment fields */}
-                <TextInput
-                  onChange={setNewComment}
-                  value={newComment}
-                  placeholder={`Comment as ${data?.authorDetails?.firstName + data?.authorDetails?.lastName}`}
-                  className="w-full h-[60] border-2 border-grayColor rounded-lg p-2 font-pmedium text-[18px] mt-10 focus:border-purpleColor"
-                  multiline={true}
-                />
-                <CustomButton
-                  title={"Submit comment"}
-                  containerStyles={"bg-purpleColor my-4 h-[50] rounded-lg"}
-                  textStyles={"text-white font-psemibold text-xl"}
-                />
-              </>
-            }
-          </View>
-        )}
+              {/* comment fields */}
+              <TextInput
+                onChangeText={(e) => setNewComment({ ...newComment, ["comment"]: e })}
+                value={newComment?.comment}
+                placeholder={`Comment as ${data?.authorDetails?.firstName + data?.authorDetails?.lastName}`}
+                className="w-full h-[60] border-2 border-grayColor rounded-lg p-2 font-pmedium text-[18px] focus:border-purpleColor mt-10"
+                multiline={true}
+              />
+              <CustomButton
+                title={"Submit comment"}
+                containerStyles={"bg-purpleColor my-4 h-[50] rounded-lg"}
+                textStyles={"text-white font-psemibold text-xl"}
+                handlePress={handlePostComment}
+              />
+            </>
+          }
+        </View>
 
-        ListEmptyComponent={() => (
-          <Text className="font-pmedium text-xl text-center">No comments</Text>
-        )}
+        {/* render comments */}
+        <View className="flex-1 pt-10">
+          {
+            comments.length > 0 ?
+              <Text className="text-xl font-psemibold pb-2 mb-3 border-b-2 border-b-darkGrayColor mx-2">All comments</Text>
+              : <></>
+          }
+          {
+            comments.length > 0 &&
+            comments.map((e, index) => (
+              <CommentCard postId={params?.postId} postAuthor={data?.author} comment={e} key={index} deleted={handleDeletedComment} />
+            ))
+          }
 
-        ListFooterComponent={() => (
-          <Text className="pt-4 text-center"><Entypo name="dots-three-horizontal" size={24} color="grey" /></Text>
-        )}
-
-        contentContainerStyle={{ paddingBottom: 30 }}
-      />
+          {loading && <ActivityIndicator size={"large"} color={"#6835F0"} />}
+        </View>
+      </ScrollView>
     </View>
   )
 }
