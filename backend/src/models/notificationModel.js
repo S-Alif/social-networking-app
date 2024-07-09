7b674bddb4bdda4ba011d5a500112627084b9649
Alif <sagor01md@gmail.com>
2024-07-09 19:46:35 +0600

    [UPDATE] MODELS: notification model

--- backend/src/models/notificationModel.js
+++ backend/src/models/notificationModel.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const notificationSchema = mongoose.Schema({
   message: { type: String, required: true },
-  notificationFrom: { type: mongoose.Types.ObjectId, required: true },
-  notificationTo: { type: mongoose.Types.ObjectId, default: true },
-  type: { type: String, enum: ["post", "request"] },
-  seen: { type: Boolean, default: 0 }
+  notificationFrom: { type: mongoose.Types.ObjectId, required: true, ref: 'users' },
+  notificationTo: { type: mongoose.Types.ObjectId, required: true, ref: 'users' },
+  type: { type: String, enum: ["post", "comment", "reaction", "request", "request_accept"] },
+  seen: { type: Boolean, default: false },
+  postId: { type: mongoose.Types.ObjectId, ref: 'posts', required: false },
+  commentId: { type: mongoose.Types.ObjectId, ref: 'comments', required: false },
+  requestId: { type: mongoose.Types.ObjectId, ref: 'requests', required: false }
 }, { timestamps: true, versionKey: false });
 
 module.exports = mongoose.model('notifications', notificationSchema)
