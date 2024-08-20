d3867bc40104f1ae0217e4e3a765fc17a2f9702c
Alif <sagor01md@gmail.com>
2024-08-20 19:23:15 +0600

    [ADD] BACKEND: required files for messaging

---
+++ backend/src/models/messageModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const messageSchema = mongoose.Schema({
+  from: { type: mongoose.Types.ObjectId, required: true, ref: "users" },
+  to: { type: mongoose.Types.ObjectId, required: true, ref: "users" },
+  message: { type: String, required: true },
+  edited: { type: Boolean, default: false },
+}, { timestamps: true, versionKey: false })
+
+module.exports = mongoose.model('messages', messageSchema)
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -6,6 +6,7 @@ const userRoutes = require('./specificRoutes/userRoutes')
 const postRoutes = require('./specificRoutes/postRoutes')
 const commentReactionRoutes = require('./specificRoutes/commentReactionRoutes')
 const notificaitonRoute = require('./specificRoutes/notificationRoute')
+const messageRoute = require('./specificRoutes/messageRoute')
 
 // middleware
 const authVerification = require('../middlewares/authVerification')
@@ -17,5 +18,6 @@ router.use('/user', userRoutes)
 router.use('/post', authVerification, postRoutes)
 router.use('/engagement', authVerification, commentReactionRoutes)
 router.use('/notification', authVerification, notificaitonRoute)
+router.use('/message', authVerification, messageRoute)
 
 module.exports = router
